b0df2b42129028c659b32d2983e956ddf006eaca
christianmbonet <christianm.bonet@gmail.com>
2021-07-07 15:40:53 -0400

    changing class names

--- src/components/CarouselImages.js
+++ src/components/CarouselImages.js
@@ -6,15 +6,6 @@ const CarouselImages = ({src}) => {
 return (
     <div>
         <img src={src} alt='carouselimages'></img>
-        {/* <div className='placeholderImageRow'>
-            <img className='degasImage' src={degas} alt='degas'></img>
-        </div>
-        <div className='placeholderImageRow'>
-            <img className='loversImage' src={lovers} alt='lovers'></img>
-        </div>
-        <div className='placeholderImageRow'>
-            <img className='caballoImage' src={caballo} alt='caballo'></img>
-        </div> */}
     </div>
 )
 
