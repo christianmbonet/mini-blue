d5f6aca024dd44b780484332849ff3437475c0c1
christianmbonet <christianm.bonet@gmail.com>
2021-07-22 14:26:09 -0400

    adding power page

---
+++ src/pages/power.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import power from '../images/power.jpg';
+import '../components/index.css';
+
+const doitPage = () => {
+    return (
+        <div className='powerPage'>
+        <img src= {power} alt='power'></img>
+    </div>
+    )
+}
+
+export default powerPage;
