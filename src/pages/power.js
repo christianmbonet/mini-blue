63dfba23736be09592ac7b1a67c55c9d43ff75ac
christianmbonet <christianm.bonet@gmail.com>
2021-07-22 14:26:34 -0400

    minor edits to power page

--- src/pages/power.js
+++ src/pages/power.js
@@ -2,7 +2,7 @@ import React from 'react';
 import power from '../images/power.jpg';
 import '../components/index.css';
 
-const doitPage = () => {
+const powerPage = () => {
     return (
         <div className='powerPage'>
         <img src= {power} alt='power'></img>
