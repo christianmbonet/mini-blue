e9053fbf3b646defce87b8a782b50aa2274f2654
christianmbonet <christianm.bonet@gmail.com>
2021-08-24 11:09:35 -0400

    adding bird page

---
+++ src/pages/bird.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import bird from '../images/bird.jpg';
+import '../components/index.css';
+import Navigation from '../components/Navigation';
+
+const birdPage = () => {
+    return (
+        <div className='itPage'>
+        <img className='bird' src= {bird} alt='bird'></img>
+        <div className='secondRowWebPage'>
+            <Navigation className='redNav'/>
+        </div>
+    </div>
+    )
+}
+
+export default birdPage;
